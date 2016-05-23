30e483320eaa0e8a34aec0fdc19c9c57333c5eea
robkitro <robkitro@happybearsoftware.com>
2016-05-23 10:32:29 +0100

    first go at temp converter

--- efp_14-23/efp_18.rb
+++ efp_14-23/efp_18.rb
@@ -15,3 +15,30 @@
 #Constraints
 # Ensure that you allow upper or lowercase values for C and F.
 # Use as few output statements as possible and avoid repeating output strings.
+
+def temp_converter
+
+puts "press C to convert from Fahrenheit to Celsius\n
+press F to convert from Celsius to Fahrenheit"
+scale = gets.chomp.upcase
+
+
+if scale == 'C'
+  puts " enter the temperature in fahrenheit:"
+  tempc = gets.chomp.to_i
+  celsius = (tempc - 32) * 5/9
+  puts " the temp in celsius is #{celsius} celsius"
+end
+
+if scale == 'F'
+  puts " enter the temperature in celsius"
+  tempf = gets.chomp.to_i
+  fahrenheit = (tempf * 9/5) + 32
+  puts "the temp in fahrenheit is #{fahrenheit} fahrenheit"
+end
+
+end
+
+
+
+temp_converter
